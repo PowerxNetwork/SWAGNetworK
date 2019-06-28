deda59cd6630c8ac6afbdb713d151dfe158ddc60
PowerxNetwork <52278911+PowerxNetwork@users.noreply.github.com>
2019-06-29 00:29:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -78,24 +78,26 @@ client.on('message', message => {
 });
 
 
-client.on('message', function(msg) {
-    if(msg.content.startsWith (prefix  + '!server')) {
-      let embed = new Discord.RichEmbed()
-      .setColor('RANDOM')
-      .setThumbnail(msg.guild.iconURL)
-      .setTitle(`Showing Details Of  **${msg.guild.name}*`)
-      .addField(':globe_with_meridians:** نوع السيرفر**',`[** __${msg.guild.region}__ **]`,true)
-      .addField(':medal:** __الرتب__**',`[** __${msg.guild.roles.size}__ **]`,true)
-      .addField(':red_circle:**__ عدد الاعضاء__**',`[** __${msg.guild.memberCount}__ **]`,true)
-      .addField(':large_blue_circle:**__ عدد الاعضاء الاونلاين__**',`[** __${msg.guild.members.filter(m=>m.presence.status == 'online').size}__ **]`,true)
-      .addField(':pencil:**__ الرومات الكتابية__**',`[** __${msg.guild.channels.filter(m => m.type === 'text').size}__** ]`,true)
-      .addField(':microphone:**__ رومات الصوت__**',`[** __${msg.guild.channels.filter(m => m.type === 'voice').size}__ **]`,true)
-      .addField(':crown:**__ الأونـر__**',`**${msg.guild.owner}**`,true)
-      .addField(':id:**__ ايدي السيرفر__**',`**${msg.guild.id}**`,true)
-      .addField(':date:**__ تم عمل السيرفر في__**',msg.guild.createdAt.toLocaleString())
-      msg.channel.send({embed:embed});
-    }
-  });
+client.on("message", message => {
+if(message.content.startsWith(prefix + "!vmute")) {
+var mnt = message.mentions.members.first();
+if(message.author.bot || message.channel.type == "dm" || !message.member.hasPermission("MUTE_MEMBERS") || !message.guild.member(client.user).hasPermission("MUTE_MEMBERS")) return;
+if(!mnt) return message.channel.send(`**• | Usage:** ${prefix}vmute \`\`@Name\`\``);
+if(!mnt.voiceChannel) return message.channel.send(`⛔ | *${mnt.user.tag}* is not in a voice channel!`);
+mnt.setMute(true).then(() => {
+message.channel.send(`Successfully Muted ${mnt} :+1:`)
+}).catch(console.error);
+}
+if(message.content.startsWith(prefix + "!unvmute")) {
+var mnt = message.mentions.members.first();
+if(message.author.bot || message.channel.type == "dm" || !message.member.hasPermission("MUTE_MEMBERS") || !message.guild.member(client.user).hasPermission("MUTE_MEMBERS")) return;
+if(!mnt) return message.channel.send(`**• | Usage:** ${prefix}unvmute \`\`@Name\`\``);
+if(!mnt.voiceChannel) return message.channel.send(`⛔ | *${mnt.user.tag}* is not in a voice channel!`);
+mnt.setMute(false).then(() => {
+message.channel.send(`Successfully Unmuted ${mnt} :+1:`)
+}).catch(console.error);
+}
+});
 
 
 client.login(process.env.BOT_TOKEN);
