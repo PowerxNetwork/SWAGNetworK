2511169dfd96a6a397767aeca43360e42f624d46
PowerxNetwork <52278911+PowerxNetwork@users.noreply.github.com>
2019-06-28 23:03:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,4 +25,15 @@ client.user.setStatus("dnd")
 });
 
 
+client.on("message", (message) => {
+if (message.content.startsWith("a!cv")) {
+            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
+        let args = message.content.split(" ").slice(1);
+    message.guild.createChannel(args.join(' '), 'voice');
+    message.channel.sendMessage('تـم إنـشاء روم صـوتي')
+    
+}
+});
+
+
 client.login(process.env.BOT_TOKEN);
