d86356331772c4f80d1b94cfb1b572101be7fe3b
PowerxNetwork <52278911+PowerxNetwork@users.noreply.github.com>
2019-06-28 23:05:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,15 +25,23 @@ client.user.setStatus("dnd")
 });
 
 
-client.on("message", (message) => {
-if (message.content.startsWith("a!cv")) {
-            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
-        let args = message.content.split(" ").slice(1);
-    message.guild.createChannel(args.join(' '), 'voice');
-    message.channel.sendMessage('تـم إنـشاء روم صـوتي')
-    
-}
-});
+client.on('message', message => {
+              if (!message.channel.guild) return;
+      if(message.content =='*member')
+      var IzRo = new Discord.RichEmbed()
+      .setThumbnail(message.author.avatarURL)
+      .setFooter(message.author.username, message.author.avatarURL) 
+      .setTitle('🌷| Members info')
+      .addBlankField(true)
+      .addField('📗| Online',
+      `${message.guild.members.filter(m=>m.presence.status == 'online').size}`)
+      .addField('📕| DND',`${message.guild.members.filter(m=>m.presence.status == 'dnd').size}`)
+      .addField('📙| Idle',`${message.guild.members.filter(m=>m.presence.status == 'idle').size}`)
+      .addField('📓| Offline',`${message.guild.members.filter(m=>m.presence.status == 'offline').size}`)
+      .addField('➡| Server Members',`${message.guild.memberCount}`)
+      message.channel.send(IzRo);
+	
+    });
 
 
 client.login(process.env.BOT_TOKEN);
