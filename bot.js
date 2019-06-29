0304debef7e6219595b3231d5cf25a0494ea1ca7
PowerxNetwork <52278911+PowerxNetwork@users.noreply.github.com>
2019-06-29 16:22:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -395,4 +395,26 @@ setInterval(() => {
 });
 
 
+const prefix = '!';
+
+client.on('message', function(msg) {
+
+    if(msg.content.startsWith (prefix + 'server')) {
+      if(!msg.channel.guild) return msg.reply('**❌ اسف لكن هذا الامر للسيرفرات فقط **');
+      let embed = new Discord.RichEmbed()
+      .setColor('#000000')
+      .setThumbnail(msg.guild.iconURL)
+      .setTitle(`${msg.guild.name}`,true)
+      .addField(':id: **Server ID:**',`${msg.guild.id}`,true)
+      .addField('📅** Created On**',msg.guild.createdAt.toLocaleString())
+      .addField('👑** Owned By**',`${msg.guild.owner}`,true)
+      .addField(':busts_in_silhouette:  **Members **' + `[ ${msg.guild.memberCount} ]`,`**${msg.guild.members.filter(m=>m.presence.status == 'online').size}**` + ' Online')
+      .addField(':speech_balloon: Channels ' + `[ ${msg.guild.channels.size} ]`,`**${msg.guild.channels.filter(m => m.type === 'text').size}**` + ' Text | ' + `**${msg.guild.channels.filter(m => m.type === 'voice').size}**` + ' Voice')//tt
+      .addField(':earth_africa: Others','**Region: **' + `${msg.guild.region}` + ' **Verification Level:** ' + `${msg.guild.verificationLevel}`)
+      .addField(':closed_lock_with_key:** Rules **' + `[ ${msg.guild.roles.size} ]`,'To see a list with all roles use **#roles**');
+      msg.channel.send({embed:embed});
+    }
+  });
+
+
 client.login(process.env.BOT_TOKEN);
