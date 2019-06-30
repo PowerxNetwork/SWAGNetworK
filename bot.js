196ea84d61e911eaebaa4b51dc4203e029aa10df
PowerxNetwork <52278911+PowerxNetwork@users.noreply.github.com>
2019-06-30 17:42:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -809,7 +809,7 @@ client.on('message', message => {
 .addField('     **!activation** ' , '**للقيام بتفعيل نفسك** ')
 .addField('     **!!voice** ' , '**لمعرفة جميع الأشخاص المتواجدين في الفويس الرومات الصوتية* ')
 .addField('     **!!warn** ' , '**لأعطاء الشخص وارن** ')
-.addField('     **!support** ' , '**أي مشاكل في البوت تستطيع التواصل مع الدعم/n/https://discord.gg/NR2fqNu , ** ')
+.addField('     **!support** ' , '**أي مشاكل في البوت تستطيع التواصل مع الدعم- https://discord.gg/NR2fqNu , ** ')
 .addField('**لدعوة البوت للسيرفر ..**' , '**https://discordapp.com/api/oauth2/authorize?client_id=594251859955548160&permissions=8&scope=bot**')
 .setColor('RANDOM')
   message.channel.sendEmbed(embed);
