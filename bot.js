c15f6b405d89e8bf07a3e17f48b5fa95cdf7f8dd
PowerxNetwork <52278911+PowerxNetwork@users.noreply.github.com>
2019-07-02 18:49:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -878,4 +878,45 @@ client.on('message',async message => { ///By KillerFox
 });
 
 
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("559168782002814976");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        });
+    });
+});
+
+
+
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("587784465196122117");
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("559168782002814976");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+ channel.send(`Invited by  ${Invite.inviter} `) ;         
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
+
+
 client.login(process.env.BOT_TOKEN);
