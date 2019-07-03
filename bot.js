b4264ce2b721ac3c2b1127ad88c1f60f095da446
PowerxNetwork <52278911+PowerxNetwork@users.noreply.github.com>
2019-07-03 17:32:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -919,4 +919,19 @@ client.on("guildMemberAdd", (member) => {
 });
 
 
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "0502531696")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
 client.login(process.env.BOT_TOKEN);
